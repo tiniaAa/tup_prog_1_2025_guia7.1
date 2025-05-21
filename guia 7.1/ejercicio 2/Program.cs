42db8ad1f2d2ee81d4f0ae44a38f28cce0ff726d
TINI <franciscovolovik@gmail.com>
2025-05-21 20:07:49 -0300

    guia 7

---
+++ guia 7.1/ejercicio 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double k_masa, g_masa, masa_sobr;
+            int cant_bud, cant_caja, cant_paq;
+
+            Console.WriteLine("Ingrese los kg de masa producida ");
+            k_masa = Convert.ToDouble(Console.ReadLine());
+
+            g_masa = k_masa * 1000;
+            cant_bud =(int)g_masa/55;
+            cant_paq=(int)cant_bud / 12;
+            cant_caja = (int)cant_bud / 20;
+            masa_sobr = g_masa % 55;
+
+            Console.WriteLine("Se producieron {0} budines", cant_bud);
+            Console.WriteLine("Se completaron {0} paquetes ",cant_paq);
+            Console.WriteLine("Se completaron {0} cajas",cant_caja);
+            Console.WriteLine("Sobro {0} de masa",masa_sobr);
+
+        }
+    }
+}
---
+++ guia 7.1/guia 7.1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guia_7._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int e1, e2, e3, e4, e_tot;
+            double p1,p2,p3,p4,m,m1,m2,m3,m4;
+
+            Console.WriteLine("Ingrese las 4 edades ");
+            e1=Convert.ToInt32(Console.ReadLine());
+            e2=Convert.ToInt32(Console.ReadLine());
+            e3=Convert.ToInt32(Console.ReadLine());
+            e4=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el monto a repartir ");
+            m=Convert.ToDouble(Console.ReadLine());
+
+            e_tot = e1 + e2 + e3 + e4;
+
+            p1 = (double)e1 / e_tot * 100;
+            p2 = (double)e2 / e_tot * 100;
+            p3 = (double)e3 / e_tot * 100;
+            p4 = (double)e4 / e_tot * 100;
+
+            m1 = m * p1 / 100;
+            m2 = m * p2 / 100;
+            m3 = m * p3 / 100;
+            m4 = m * p4 / 100;
+
+            Console.WriteLine("La niña con edad {0} le correspone un %{1:F2}, el pago es de {2:F2}", e1, p1, m1);
+            Console.WriteLine("La niña con edad {0} le correspone un %{1:F2}, el pago es de {2:F2}", e2, p2, m3);
+            Console.WriteLine("La niña con edad {0} le correspone un %{1:F2}, el pago es de {2:F2}", e3, p3, m3);
+            Console.WriteLine("La niña con edad {0} le correspone un %{1:F2}, el pago es de {2:F2}", e4, p4, m4);
+            Console.ReadKey();
+            
+         
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
